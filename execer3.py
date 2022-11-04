875cbb0ca3b11cd869b82065a557a857c3912282
LuizMessias123 <117416114+LuizMessias123@users.noreply.github.com>
2022-11-03 22:23:44 -0300

    Add files via upload

---
+++ execer3.py
@@ -0,0 +1,12 @@
+#trabalhan com variaveis
+#num1 = 10
+#num2 = 5
+
+num1 = input('informe o valor:')
+num2 = input('informe o valor:')
+
+print(type(num1))
+
+soma = num1 + num2
+soma1 = int(num1) + int(num2)
+print('A soma é: {}', format(soma1))
