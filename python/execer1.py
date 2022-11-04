0a651c08805c4f59f67bf6d68f3ec7d83164c4c5
LuizMessias123 <117416114+LuizMessias123@users.noreply.github.com>
2022-11-03 22:02:53 -0300

    Add files via upload

---
+++ python/execer1.py
@@ -0,0 +1,4 @@
+#caracter de comentario
+#print para imprimir informações na tela 
+print('\nOlá mundo python\n')
+print('Meu nome é: ', 'Luiz Messias,', 18 , 'anos\n' )
---
+++ python/exerc2.py
@@ -0,0 +1,3 @@
+#usando imput para entrada de dados
+print('\nSeja bem vindo a aula de', 'python\n')
+print('Olá {}'. format(input('\n\tqual é o seu nome?\n')))
