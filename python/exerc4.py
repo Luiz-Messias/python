ccc1caf311e584fb1fb4fd9419f35da8aefd240d
Luiz Messias <117416114+LuizMessias123@users.noreply.github.com>
2022-11-08 21:17:06 -0300

    Add files via upload

---
+++ python/exerc4.py
@@ -0,0 +1,10 @@
+num1 = int(input('Insira um numero:'))
+num2 = int(input('Insira um numero:'))
+
+multi1 = (num1) * (num2)
+
+print('A multilplicação é: {}' . format(multi1))
+
+div1 = (num1) / (num2)
+
+print('A divisão é: {}' . format(div1))
